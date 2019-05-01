2f220e5cf48af03903ee3f2763e299e115f69a39
EvanBell <876895046@qq.com>
2019-05-01 21:47:11 +0800

    添加babel配置

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import _ from 'lodash';
 import './test.scss';
 
-function component() {
+const component = () => {
     const element = document.createElement('div');
     // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.innerHTML = _.join(['Hello', 'webpack + scss + bebel'], ' ');
     return element;
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -7,13 +7,18 @@ module.exports = {
     path: path.resolve(__dirname, 'dist')
   },
   module: {
-    rules: [{
+    rules: [
+      { 
+        test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" 
+      },
+      {
         test: /\.scss$/,
         use: [
             "style-loader", // creates style nodes from JS strings
             "css-loader", // translates CSS into CommonJS
             "sass-loader" // compiles Sass to CSS, using Node Sass by default
         ]
-    }]
-}
+      }
+    ]
+  }
 };
